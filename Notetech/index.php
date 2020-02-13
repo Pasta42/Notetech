127cf2544e2d70c233c101ecdf82535c7f017976
Pasta42 <sites.pasta42@gmail.com>
2020-02-12 22:56:21 -0300

    add site notetech dia 12-02-2020 com section de parceiros

---
+++ Notetech/index.php
@@ -0,0 +1,332 @@
+    <!DOCTYPE html>
+    <html lang="pt-br">
+      <head>
+        <title>NoteTech Informática</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" href="favicon.ico" sizes="16x16" type="image/png">
+        <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,700" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+        <link rel="stylesheet" href="css/animate.css">
+        
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+        <link rel="stylesheet" href="css/owl.theme.default.min.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">
+
+        <link rel="stylesheet" href="css/aos.css">
+
+        <link rel="stylesheet" href="css/ionicons.min.css">
+
+        <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+        <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+        
+        <link rel="stylesheet" href="css/flaticon.css">
+        <link rel="stylesheet" href="css/icomoon.css">
+        <link rel="stylesheet" href="css/style.css">
+      </head>
+      <body>
+
+        
+        <div class="page">
+      
+        
+        <div id="colorlib-page">
+          <header>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="colorlib-navbar-brand">
+                    <a class="colorlib-logo" href="index.php"><span class="logo-img" style="background-image: url(img/logo.png);"></span>NoteTech</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </header>
+        
+          <section class="home-slider owl-carousel js-fullheight">
+            <div class="slider-item js-fullheight">
+              <div class="overlay"></div>
+              <div class="container">
+                <div class="row slider-text align-items-center text-center justify-content-center" data-scrollax-parent="true">
+                  <div class="col-md-12 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+                    <p><a class="scroll">Bem Vindo a</a></p>
+                    <h1 class="mb-3" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">NoteTech Informática</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="slider-item js-fullheight">
+          <div class="overlay"></div>
+          <div class="container">
+            <div class="row slider-text align-items-center text-center justify-content-center" data-scrollax-parent="true">
+              <div class="col-md-12 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+                <p><a class="scroll">Assistência</a></p>
+                <h1 class="mb-3" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"> Técnica</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="slider-item js-fullheight">
+          <div class="overlay"></div>
+          <div class="container">
+            <div class="row slider-text align-items-center text-center justify-content-center" data-scrollax-parent="true">
+              <div class="col-md-12 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+                <p><a class="scroll">Eletrônica</a></p>
+                <h1 class="mb-3" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Especializada</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="slider-item js-fullheight">
+          <div class="overlay"></div>
+          <div class="container">
+            <div class="row slider-text align-items-center text-center justify-content-center" data-scrollax-parent="true">
+              <div class="col-md-12 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+                <p><a href="#" class="scroll">Venda de </a></p>
+                <h1 class="mb-3" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Equipamentos e Periféricos</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="slider-item js-fullheight">
+          <div class="overlay"></div>
+          <div class="container">
+            <div class="row slider-text align-items-center text-center justify-content-center" data-scrollax-parent="true">
+              <div class="col-md-12 ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+                <p><a class="scroll">Configurações de </a></p>
+                <h1 class="mb-3" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Redes</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+            
+          </section>
+
+          <div class="container">
+          <div class="overlay"></div>
+            <div class="row slider-text align-items-center text-center justify-content-center" data-scrollax-parent="true">
+              <div class="col-md-12 col-lg-12 col-sm-12 text-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+                <img class="img-fluid mb-3" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }" src="img/capa.jpeg" alt="Capa">
+              </div>
+            </div>
+          </div>
+          <!-- END slider -->
+
+          <section class="ftco-section about-section">
+            <div class="container">
+              <div class="row d-flex" data-scrollax-parent="true">
+                <div class="col-md-4 author-img" style="background-image: url(img/fachada1.jpeg);" data-scrollax=" properties: { translateY: '-70%'}"></div>
+                <div class="col-md-2"></div>
+                <div class="col-md-6 wrap ftco-animate">
+                  <div class="about-desc">
+                    <h1 class="bold-text">Sobre</h1>
+                    <div class="p-5">
+                      <h2 class="mb-5">Nós somos a Notetech</h2>
+                      <p>Uma empresa voltada para dar suporte e manutenção em equipamentos eletrônicos, levando sempre o compromisso de atender o nosso cliente e alcançar a sua satisfação.</p>
+                      <p><a>Nossas Redes Sociais</a></p>
+                      <ul class="ftco-footer-social list-unstyled mt-4">
+                        <li><a href="https://api.whatsapp.com/send?phone=+5555999769845" target="_blank"><span class="icon-whatsapp"></span></a></li>
+                        <li><a href="https://www.facebook.com/notetechinformatica/?ref=br_rs" target="_blank"><span class="icon-facebook"></span></a></li>
+                        <li><a href="https://www.instagram.com/notetechinformatica/?hl=pt-br" target="_blank"><span class="icon-instagram"></span></a></li>
+                      </ul>
+                      <h5>Contate-nos</h5>
+                      <p>Email: <a>contato@notetechinformatica.com.br</a></p>
+                      <p>Fone: <a>(55) 99976-9845</a></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section class="ftco-section">
+            <div class="container">
+              <div class="row justify-content-center mb-5 pb-5">
+                <div class="col-md-7 text-center heading-section ftco-animate">
+                  <span>Com o que nós</span>
+                  <h2>Trabalhamos</h2>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+                  <div class="media block-6 services p-3 py-4 d-block text-center">
+                    <div class="icon mb-3"><span class="icon-gears"></span></div>
+                    <div class="media-body">
+                      <h3 class="heading">Suporte e manutenção em equipamentos de TI</h3>
+                      <h3 class="heading">Ex.:computadores, impressoras, roteadores, etc.</h3>
+                      <h3 class="heading"></h3>
+                    </div>
+                  </div>      
+                </div>
+                <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+                  <div class="media block-6 services p-3 py-4 d-block text-center">
+                    <div class="icon mb-3"><span class="icon-wifi"></span></div>
+                    <div class="media-body">
+                      <h3 class="heading">Configuração e instalação de redes de pequeno a grande porte</h3>
+                      <h3 class="heading">Configurações de redes domésticas à empresariais</h3>
+                      <h3 class="heading"></h3>
+                    </div>
+                  </div>      
+                </div>
+                <div class="col-md-4 d-flex align-self-stretch ftco-animate">
+                  <div class="media block-6 services p-3 py-4 d-block text-center">
+                    <div class="icon mb-3"><span class="icon-layers"></span></div>
+                    <div class="media-body">
+                      <h3 class="heading">Serviçõs de eletrônica em TI</h3>
+                      <h3 class="heading">Notebooks, Desktops, Impressoras, etc.</h3>
+                      <h3 class="heading"></h3>
+                    </div>
+                  </div>    
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <section class="ftco-section">
+            <div class="container">
+              <div class="row justify-content-center mb-5 pb-5">
+                <div class="col-md-7 text-center heading-section ftco-animate">
+                  <span>Nossos Serviços</span>
+                  <h2>Uma breve explicação</h2>
+                </div>
+              </div>
+              <div class="row no-gutters">
+              <div class="block-3 d-md-flex ftco-animate" data-scrollax-parent="true">
+                <a class="image d-flex justify-content-center align-items-center" style="background-image: url('img/suporte.jpg'); " data-scrollax=" properties: { translateY: '-30%'}">
+                </a>
+                <div class="text">
+                  <h2 class="heading"><a>Suporte</a></h2>
+                  <p>Realizamos manutenção preventiva, atualização de softwares e sistemas operacionais. realizamos acessos remotos e configuramos máquinas, dispositivos, sistemas operacionais e softwares mesmo à distância.</p>
+                </div>
+              </div>
+              <div class="block-3 d-md-flex ftco-animate" data-scrollax-parent="true">
+                <a class="image order-2 d-flex justify-content-center align-items-center" style="background-image: url('img/redes.jpg'); " data-scrollax=" properties: { translateY: '-30%'}">
+                </a>
+                <div class="text order-1">
+                  <h2 class="heading"><a></a>Configuração de Redes</h2>
+                  <p>Realizar a configuração da rede é bem mais que apenas conectar cabos. Garantimos aos nossos clientes que possuem a melhor configuração visando sempre a otimização da rede para o que for contratado, nosso compromisso é sua comodidade.</p>
+                </div>
+              </div>
+              <div class="block-3 d-md-flex ftco-animate" data-scrollax-parent="true">
+                <a class="image d-flex justify-content-center align-items-center" style="background-image: url('img/loja.jpeg'); " data-scrollax=" properties: { translateY: '-30%'}">
+                </a>
+                <div class="text">
+                  <h2 class="heading"><a>Venda de Equipamentos</a></h2>
+                  <p>Em nossa loja nós temos todos os materiais necessários para suprir a sua empresa e também todos os acessórios e periféricos para dar um UP! no seu equipamento.</p>
+                </div>
+              </div>
+              <div class="block-3 d-md-flex ftco-animate" data-scrollax-parent="true">
+                <a class="image order-2 d-flex justify-content-center align-items-center" style="background-image: url('images/work-4.jpg'); " data-scrollax=" properties: { translateY: '-30%'}">
+                </a>
+                <div class="text order-1">
+                  <h4 class="subheading">Notebooks</h4>
+                  <h2 class="heading"><a>Manutenção e Serviços</a></h2>
+                  <p>Realizamos a formatação e instalação de Sistemas Operacionais, limpeza lógica e limpeza física, troca de peças e manutenção preventiva.</p>
+                </div>
+              </div>
+              <div class="block-3 d-md-flex ftco-animate" data-scrollax-parent="true">
+                <a class="image d-flex justify-content-center align-items-center" style="background-image: url('img/desktop.jpg'); " data-scrollax=" properties: { translateY: '-30%'}">
+                </a>
+                <div class="text">
+                  <h4 class="subheading">Desktop</h4>
+                  <h2 class="heading"><a>Manutenção e Serviços</a></h2>
+                  <p>Para Desktops ou Computadores de mesa nós realizamos a formatação e instalação de Sistemas Operacionais, limpeza lógica e limpeza física, troca de peças e manutenção preventiva.</p>
+                </div>
+              </div>
+              <div class="block-3 d-md-flex ftco-animate" data-scrollax-parent="true">
+                <a class="image order-2 d-flex justify-content-center align-items-center" style="background-image: url('img/impressora.jpg'); " data-scrollax=" properties: { translateY: '-30%'}">
+                </a>
+                <div class="text order-1">
+                  <h4 class="subheading">Impressoras</h4>
+                  <h2 class="heading"><a>Manutenção e Serviços</a></h2>
+                  <p>Realizar uma limpeza preventiva em caso de impressoras pode significar muitas vezes prolongar a sua vida útil, e nós realizamos para você não somente isso, como também podemos fazer troca de fonte queimada, serviço de conserto de placa, reset da placa e também alinhamento de cartuchos, limpeza de cabeçotes e muitos outros serviços voltados a impressora, sempre buscando a eficácia e sua satisfação.</p>
+                </div>
+              </div>
+              <div class="block-3 d-md-flex ftco-animate" data-scrollax-parent="true">
+                <a class="image d-flex justify-content-center align-items-center" style="background-image: url('img/eletronica.jpg'); " data-scrollax=" properties: { translateY: '-30%'}">
+                </a>
+                <div class="text">
+                  <h2 class="heading"><a>Serviços de Eletrônica</a></h2>
+                  <p>Possuímos vasto conhecimento e experiência em consertos de equipamentos eletrônicos, realizamos trocas de capacitores, transistores, reset de BIOS, reballing, troca de fusíveis e tudo com garantia NoteTech de aprovação.</p>
+                </div>
+                </div>
+            </div>
+            </div>
+           
+        <div class="container">
+          <div class="overlay"></div>
+            <div class="row slider-text align-items-center text-center justify-content-center"      data-scrollax-parent="true">
+              <div class="col-md-12 col-lg-12 col-sm-12 text-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+                <h1>Nossos Parceiros</h1>
+               <a href="http://pasta42.com.br/" target="_blank">
+                <img id="pasta42" class="img-fluid mb-3" href="http://pasta42.com.br/" style="margin-top:18px;" src="img/logo-novo-pasta.png" alt="Capa"></a>
+              </div>
+            </div>
+          </div>
+
+          <footer class="ftco-footer ftco-bg-dark ftco-section">
+            <div class="container">
+              <div class="row mb-5 justify-content-center">
+                <div class="col-md-5 text-center">
+                  <div class="ftco-footer-widget mb-5">
+                    <ul class="ftco-footer-social list-unstyled">
+                      <li><a href="https://api.whatsapp.com/send?phone=+5555999769845" target="_blank"><span class="icon-whatsapp"></span></a></li>
+                      <li class="ftco-animate"><a href="https://www.facebook.com/notetechinformatica/?ref=br_rs" target="_blank"><span class="icon-facebook"></span></a></li>
+                      <li class="ftco-animate"><a href="https://www.instagram.com/notetechinformatica/?hl=pt-br" target="_blank"><span class="icon-instagram"></span></a></li>
+                    </ul>
+                  </div>
+                  <div class="ftco-footer-widget" style="word-wrap: break-word;">
+                    <h2 class="mb-3">Nosso Contato</h2>
+                    <p class="h3 email"><a>contato@notetechinformatica.com.br</a></p>
+                    <p class="h3 fone">Fone: <a>(55) 99976-9845</a></p>
+                    <p class="h3 end"><a>Rua Silveira Martins, 459   Santana do Livramento - RS</a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12 text-center">
+
+                  <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+          &copy;<script>document.write(new Date().getFullYear());</script> Desenvolvido por <a href="http://pasta42.com.br/" target="_blank">Pasta 42</a> 
+          <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                </div>
+              </div>
+            </div>
+          </footer>
+
+          <!-- loader -->
+          <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+          </div>
+
+        </div>
+
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jquery-migrate-3.0.1.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/jquery.easing.1.3.js"></script>
+        <script src="js/jquery.waypoints.min.js"></script>
+        <script src="js/jquery.stellar.min.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/jquery.magnific-popup.min.js"></script>
+        <script src="js/aos.js"></script>
+        <script src="js/jquery.animateNumber.min.js"></script>
+        <script src="js/scrollax.min.js"></script>
+        <script src="js/bootstrap-datepicker.js"></script>
+        <script src="js/jquery.timepicker.min.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+        <script src="js/google-map.js"></script>
+        <script src="js/main.js"></script>
+        
+      </body>
+    </html>
+
+
